510a5d8f3e2ffd47b414c0fc98ff6268f75e880e
Sooraj Vijayan <sooraj.vijayan@bps.uk.atos.net>
2022-09-12 15:44:47 +0000

    feat: PN-81660-Put-to-Post

--- app/controllers/contribution-header-controller.ts
+++ app/controllers/contribution-header-controller.ts
@@ -1,46 +1,51 @@
 import {
   Body,
   Get,
-  Put,
+  Post,
   Response,
   Route,
   Security,
-  SuccessResponse
+  SuccessResponse,
 } from "tsoa";
 import ContributionHeader from "../models/contributionheader";
-import { ContributionHeaderResponse, SearchResultsetResponse, HeaderFilterElements } from "../schemas/response-schema";
+import {
+  ContributionHeaderResponse,
+  SearchResultsetResponse,
+  HeaderFilterElements,
+} from "../schemas/response-schema";
 import Status from "../utils/config";
 import app from "../utils/app";
 import sequelize from "../utils/database";
 import { headerFilterParams } from "../utils/constants";
 
-@Route('/contribution')
+@Route("/contribution")
 export class ContributionHeaderController {
-
   /**
-  * 5102 API Catalogue Number
-  * Retrieves the Contribution Header list based on scheduleReference passed.
-  * @param externalScheduleRef scheduleReference of the Contribution Header record to be fetched
-  * @return Contribution Header list with Array< ContributionHeader> based on scheduleReference
-  */
+   * 5102 API Catalogue Number
+   * Retrieves the Contribution Header list based on scheduleReference passed.
+   * @param externalScheduleRef scheduleReference of the Contribution Header record to be fetched
+   * @return Contribution Header list with Array< ContributionHeader> based on scheduleReference
+   */
   @Security("api_key")
   @Get("{externalScheduleRef}")
   @SuccessResponse("200", Status.SUCCESS_MSG)
   @Response("400", Status.BAD_REQUEST_MSG)
   @Response("404", Status.NOT_FOUND_MSG)
   @Response("500", Status.FAILURE_MSG)
-  async getContributionHeader(externalScheduleRef: string): Promise<ContributionHeaderResponse | any> {
+  async getContributionHeader(
+    externalScheduleRef: string
+  ): Promise<ContributionHeaderResponse | any> {
     try {
       let whereCdtn = {
-        external_schedule_ref: externalScheduleRef
+        external_schedule_ref: externalScheduleRef,
       };
       return await sequelize.transaction(async (t) => {
         const items = await ContributionHeader.findOne({
           where: whereCdtn,
-          transaction: t
+          transaction: t,
         });
         if (items) {
-          return { ContributionHeader: items.toJSON() }
+          return { ContributionHeader: items.toJSON() };
         } else {
           return Status.BAD_REQUEST;
         }
@@ -53,12 +58,12 @@ export class ContributionHeaderController {
   }
 
   /**
-  * 5104 API Catalogue Number
-  * Retrieves a list of header with filter criterias
-  * @return Contribution Header list with Array<Contribution_Header>
-  */
+   * 5104 API Catalogue Number
+   * Retrieves a list of header with filter criterias
+   * @return Contribution Header list with Array<Contribution_Header>
+   */
   @Security("api_key")
-  @Put("/retrievescheduleheaders")
+  @Post("/retrievescheduleheaders")
   @SuccessResponse("200", Status.SUCCESS_MSG)
   @Response("400", Status.BAD_REQUEST_MSG)
   @Response("404", Status.NOT_FOUND_MSG)
@@ -82,7 +87,10 @@ export class ContributionHeaderController {
           order: element.options.sort,
           distinct: true,
           include: [
-            { association: "rdschedulestatus", attributes: ["scheduleStatusDesc"] },
+            {
+              association: "rdschedulestatus",
+              attributes: ["scheduleStatusDesc"],
+            },
           ],
           where: whereCdtn,
           subQuery: false,
--- app/controllers/member-contribution-details-controller.ts
+++ app/controllers/member-contribution-details-controller.ts
@@ -2,39 +2,53 @@ import {
   Body,
   Get,
   Put,
+  Post,
   Response,
   Route,
   Security,
-  SuccessResponse
+  SuccessResponse,
 } from "tsoa";
 import ContributionDetails from "../models/contributionDetails";
-import { DetailsFilterElements, MemberContributionDetailsResponse, SearchResultsetResponse } from "../schemas/response-schema";
-import { contributionDetailsUpdateHelper, ContributionDetailsUpdateError } from "../models";
+import {
+  DetailsFilterElements,
+  MemberContributionDetailsResponse,
+  SearchResultsetResponse,
+} from "../schemas/response-schema";
+import {
+  contributionDetailsUpdateHelper,
+  ContributionDetailsUpdateError,
+} from "../models";
 import Status from "../utils/config";
 import sequelize from "../utils/database";
 import app from "../utils/app";
-import { ContributionDetailsRequestDTO, ContributionDetailsResponseDTO, ContributionMemberDetails } from "../schemas"
-import { Context } from "@azure/functions"
-import { memberFilterParams, errorDetails, READONLY_CONTRIBUTION_DETAILS_COLUMNS_FOR_UPDATE } from "../utils/constants";
+import {
+  ContributionDetailsRequestDTO,
+  ContributionDetailsResponseDTO,
+  ContributionMemberDetails,
+} from "../schemas";
+import { Context } from "@azure/functions";
+import {
+  memberFilterParams,
+  errorDetails,
+  READONLY_CONTRIBUTION_DETAILS_COLUMNS_FOR_UPDATE,
+} from "../utils/constants";
 import errorHandler from "../utils/errorHandler";
 
-
-@Route('/contribution')
+@Route("/contribution")
 export class MemberContributionDetailsController {
-
-  private _context: Context
+  private _context: Context;
 
   constructor(context?: Context) {
     if (context) {
       this._context = context;
     }
-    this.log(`MemberContributionDetailsController is created`)
+    this.log(`MemberContributionDetailsController is created`);
   }
 
   /**
-     * This is helper to log kafka steps
-     * @param args 
-     */
+   * This is helper to log kafka steps
+   * @param args
+   */
   private log(...args: any[]) {
     if (this._context) {
       this._context.log("MemberContributionDetailsController::", ...args);
@@ -42,29 +56,31 @@ export class MemberContributionDetailsController {
   }
 
   /**
-  * 5202 API Catalogue Number
-  * Retrieves the Member Contribution Details list based on scheduleReference passed.
-  * @param nestScheduleRef scheduleReference of the Member Contribution Details record to be fetched
-  * @return Member Contribution Details list with Array< MemberContributionDetails> based on scheduleReference
-  */
+   * 5202 API Catalogue Number
+   * Retrieves the Member Contribution Details list based on scheduleReference passed.
+   * @param nestScheduleRef scheduleReference of the Member Contribution Details record to be fetched
+   * @return Member Contribution Details list with Array< MemberContributionDetails> based on scheduleReference
+   */
   @Security("api_key")
   @Get("{nestScheduleRef}")
   @SuccessResponse("200", Status.SUCCESS_MSG)
   @Response("400", Status.BAD_REQUEST_MSG)
   @Response("404", Status.NOT_FOUND_MSG)
   @Response("500", Status.FAILURE_MSG)
-  async getMemberContributionDetails(nestScheduleRef: string): Promise<MemberContributionDetailsResponse | any> {
+  async getMemberContributionDetails(
+    nestScheduleRef: string
+  ): Promise<MemberContributionDetailsResponse | any> {
     try {
       let whereCdtn = {
-        nest_schedule_ref: nestScheduleRef
+        nest_schedule_ref: nestScheduleRef,
       };
       return await sequelize.transaction(async (t) => {
         const items = await ContributionDetails.findOne({
           where: whereCdtn,
-          transaction: t
+          transaction: t,
         });
         if (items) {
-          return { ContributionDetail: items.toJSON() }
+          return { ContributionDetail: items.toJSON() };
         } else {
           return Status.BAD_REQUEST;
         }
@@ -77,12 +93,12 @@ export class MemberContributionDetailsController {
   }
 
   /**
-  * 5205 API Catalogue Number
-  * Retrieves a list of details with filter criterias
-  * @return Contribution Details list with Array<Contribution_Details>
-  */
+   * 5205 API Catalogue Number
+   * Retrieves a list of details with filter criterias
+   * @return Contribution Details list with Array<Contribution_Details>
+   */
   @Security("api_key")
-  @Put("/retrieveContributionDetails")
+  @Post("/retrieveContributionDetails")
   @SuccessResponse("200", Status.SUCCESS_MSG)
   @Response("400", Status.BAD_REQUEST_MSG)
   @Response("404", Status.NOT_FOUND_MSG)
@@ -96,6 +112,7 @@ export class MemberContributionDetailsController {
         memberFilterParams,
         "MCD"
       );
+      console.log("yes coming");
       let whereCdtn = {
         ...element.params,
       };
@@ -106,8 +123,14 @@ export class MemberContributionDetailsController {
           order: element.options.sort,
           distinct: true,
           include: [
-            { association: "rdschedulememberstatus", attributes: ["schdlMembStatusDesc"] },
-            { association: "rdpartcontribreason", attributes: ["reasonDescription"] },
+            {
+              association: "rdschedulememberstatus",
+              attributes: ["schdlMembStatusDesc"],
+            },
+            {
+              association: "rdpartcontribreason",
+              attributes: ["reasonDescription"],
+            },
           ],
           where: whereCdtn,
           subQuery: false,
@@ -126,9 +149,9 @@ export class MemberContributionDetailsController {
   }
 
   /**
-  * 5204 API Catalogue Number
-  * Update the Contribution Details
-  */
+   * 5204 API Catalogue Number
+   * Update the Contribution Details
+   */
   @Security("api_key")
   @Put("/contributionDetails")
   @SuccessResponse("200", Status.SUCCESS_MSG)
@@ -138,32 +161,45 @@ export class MemberContributionDetailsController {
   async updateMemberContributionDetails(
     @Body() requestObj: ContributionDetailsRequestDTO
   ): Promise<ContributionDetailsResponseDTO | any> {
-    this.log(`Update payload is validating...`)
+    this.log(`Update payload is validating...`);
     //Check Payload is having contributionDetail array and having items
     let allErrors: Array<ContributionDetailsUpdateError> = [];
     const transaction = await sequelize.transaction();
     try {
-      if (!(requestObj.contributionDetail instanceof Array) || requestObj.contributionDetail.length === 0) {
+      if (
+        !(requestObj.contributionDetail instanceof Array) ||
+        requestObj.contributionDetail.length === 0
+      ) {
         this.log("contributionDetail array is empty");
         allErrors.push({
           statusCode: Status.BAD_REQUEST,
           errorCode: errorDetails.CIA0600[0],
-          errorDetail: errorDetails.CIA0600[1]
+          errorDetail: errorDetails.CIA0600[1],
         });
       } else {
-        const allMemberDetailsAry = requestObj.contributionDetail as ContributionMemberDetails[]
+        const allMemberDetailsAry =
+          requestObj.contributionDetail as ContributionMemberDetails[];
         const length = allMemberDetailsAry.length;
 
         // Update member details one by one
         for (let index = 0; index < length; index++) {
-          const currentMemberDetails = allMemberDetailsAry[index] as ContributionMemberDetails
+          const currentMemberDetails = allMemberDetailsAry[
+            index
+          ] as ContributionMemberDetails;
           const membContribDetlId = currentMemberDetails.membContribDetlId;
 
           // Delete other readonly properties
           READONLY_CONTRIBUTION_DETAILS_COLUMNS_FOR_UPDATE.forEach((key) => {
-            delete currentMemberDetails[key]
+            delete currentMemberDetails[key];
           });
-          allErrors = await contributionDetailsUpdateHelper(membContribDetlId, currentMemberDetails, index, allErrors, transaction, this._context);
+          allErrors = await contributionDetailsUpdateHelper(
+            membContribDetlId,
+            currentMemberDetails,
+            index,
+            allErrors,
+            transaction,
+            this._context
+          );
           if (allErrors.length > 0) {
             break;
           }
@@ -173,30 +209,31 @@ export class MemberContributionDetailsController {
       let errorResp = {
         statusCode: Status.FAILURE,
         errorCode: errorDetails.CIA0602[0],
-        errorDetail: errorDetails.CIA0602[1]
-      }
-      if (error.name == 'CONTRIBUTION_DETAILS_VALIDATION_FAILED') {
+        errorDetail: errorDetails.CIA0602[1],
+      };
+      if (error.name == "CONTRIBUTION_DETAILS_VALIDATION_FAILED") {
         errorResp = {
           statusCode: Status.BAD_REQUEST,
           errorCode: errorDetails.CIA0600[0],
-          errorDetail: errorDetails.CIA0600[1]
-        }
+          errorDetail: errorDetails.CIA0600[1],
+        };
       }
-      this.log(`updateMemberContributionDetails is failed. Reason: ${error?.message} - ${error?.name} - ${error?.moreDetails}`);
-      allErrors.push(errorResp)
+      this.log(
+        `updateMemberContributionDetails is failed. Reason: ${error?.message} - ${error?.name} - ${error?.moreDetails}`
+      );
+      allErrors.push(errorResp);
     }
-
 
     if (allErrors.length === 0) {
       this.log("Success: No Error present, updates are commiting.");
       transaction.commit();
       return {
-        status: Status.SUCCESS, /* Defaults to 200 */
+        status: Status.SUCCESS /* Defaults to 200 */,
         body: { message: "Contribution Details are updated." },
         headers: {
           "Content-Type": "application/json",
         },
-      }
+      };
     } else {
       this.log("Error: Updates are rollbacking.");
       transaction.rollback();
