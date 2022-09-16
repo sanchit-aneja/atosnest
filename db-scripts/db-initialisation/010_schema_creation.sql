13f30e05e00afd6d195f31a37cdeb8b0b7ad3f01
Naga-Harish Muvva <naga-harish.muvva@bps.uk.atos.net>
2022-09-16 10:32:18 +0000

    feat: changed db name to contribution

--- db-scripts/db-initialisation/010_schema_creation.sql
+++ db-scripts/db-initialisation/010_schema_creation.sql
@@ -1,6 +1,6 @@
 -- Creates contribution_index_schema schema, and provisions the coi_functionapp_user user to connect and execute DML statements
 
 CREATE SCHEMA contribution_index_schema;
-GRANT CONNECT ON DATABASE contribution_index TO coi_functionapp_user;
+GRANT CONNECT ON DATABASE contribution TO coi_functionapp_user;
 GRANT USAGE ON SCHEMA contribution_index_schema TO coi_functionapp_user;
 GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA contribution_index_schema TO coi_functionapp_user;
