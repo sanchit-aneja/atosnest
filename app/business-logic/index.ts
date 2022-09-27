7ec3e92fa4d819ddcb27ea900f698fd296ce8de0
Naga-Harish Muvva <naga-harish.muvva@bps.uk.atos.net>
2022-09-27 13:17:39 +0000

    feat: PN-83615 save member details from csv file

--- app/business-logic/index.ts
+++ app/business-logic/index.ts
@@ -1,2 +1,4 @@
 export {default as Type2AValidations} from './type2AValidation';
 export {default as Type2BValidations} from './type2BValidations';
+
+export {default as SaveContributionDetails} from './saveContributionDetails';
