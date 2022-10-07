56e9b7bf546fb29b3ee19a657572f9e5867d475a
Sanchit Aneja <sanchit.aneja@bps.uk.atos.net>
2022-10-07 10:53:59 +0000

    fix: Resolve PN-69116 "Ctb index file upload validate body type2c query rebased"

--- app/business-logic/index.ts
+++ app/business-logic/index.ts
@@ -1,4 +1,5 @@
-export {default as Type2AValidations} from './type2AValidation';
+export {default as Type2AValidations} from './type2AValidations';
 export {default as Type2BValidations} from './type2BValidations';
-
+export {default as Type2CValidations} from './type2CValidations';
 export {default as SaveContributionDetails} from './saveContributionDetails';
+
