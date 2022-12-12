dd262bc6611607ab1819cba4290e9adc0cc2ea3d
Tushar Nikumbh <tushar.nikumbh@bps.uk.atos.net>
2022-12-12 16:21:58 +0000

    feat: Resolve PN-117487 "Ctb index manual entry remove 5102 and 5202"

--- app/controllers/contribution-header-controller.ts
+++ app/controllers/contribution-header-controller.ts
@@ -59,42 +59,6 @@ export class ContributionHeaderController {
       this._context.log("ContributionHeaderController::", ...args);
     }
   }
-  /**
-   * 5102 API Catalogue Number
-   * Retrieves the Contribution Header list based on scheduleReference passed.
-   * @param externalScheduleRef scheduleReference of the Contribution Header record to be fetched
-   * @return Contribution Header list with Array< ContributionHeader> based on scheduleReference
-   */
-  @Security("api_key")
-  @Get("{externalScheduleRef}")
-  @SuccessResponse("200", Status.SUCCESS_MSG)
-  @Response("400", Status.BAD_REQUEST_MSG)
-  @Response("404", Status.NOT_FOUND_MSG)
-  @Response("500", Status.FAILURE_MSG)
-  async getContributionHeader(
-    externalScheduleRef: string
-  ): Promise<ContributionHeaderResponse | any> {
-    try {
-      let whereCdtn = {
-        external_schedule_ref: externalScheduleRef,
-      };
-      return await sequelize.transaction(async (t) => {
-        const items = await ContributionHeader.findOne({
-          where: whereCdtn,
-          transaction: t,
-        });
-        if (items) {
-          return { ContributionHeader: items.toJSON() };
-        } else {
-          return Status.BAD_REQUEST;
-        }
-      });
-    } catch (err) {
-      if (err) {
-        return app.errorHandler(err);
-      }
-    }
-  }
 
   /**
    * 5104 API Catalogue Number
